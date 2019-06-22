ba2fdda833ddfe853c7399d86272ac02cf3e9449
Joey Windfelder <45344961+Windy315@users.noreply.github.com>
2019-06-22 16:03:20 -0400

    Add files via upload

---
+++ Source/Server-Launch-Manager V1.0.1.0.bat
@@ -0,0 +1,352 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFBFbSR2VAE+1EbsQ5+n//NaOr0waUec7cZvkiIvZcrVBvRGzJc976Utiq/QFDxRWMBuoYW8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSjk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhZksaHUrSXA==
+::ZQ05rAF9IBncCkqN+0xwdVsFAlTMbSXqZg==
+::ZQ05rAF9IAHYFVzEqQIDLQlCSRbPEGqvCLYUpcX64euAp199
+::eg0/rx1wNQPfEVWB+kM9LVsJDDeHLn2/FPgw6f317OLKj0wTVeU6au8=
+::fBEirQZwNQPfEVWB+kM9LVsJDDeHLn2/FPgw6f317OLKj0wTVeU6au8=
+::cRolqwZ3JBvQF1fEqQIcKQ5aTwyHLiucCadczuHt6sfHkUgPQucta+8=
+::dhA7uBVwLU+EWH2L4UA8LVt6SQODKGKsA/Ux7eechoAIfZBR2z3ixWtUBWm5HO2cNZhrqkuiCafVRxYsg8SFbZ10s0q7drM31g==
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATE02wFaDxxYiGwHUf6NoA+xMHYr8augWgzZ8d/TorJyb2BLqBAig==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdDeDJGqB5lQ1Olt4TRGMP2P6K7QS6e/+/dax8wNNGrNxKLPo36aYJfJexkTxe5Ulm1dblsoLCQkYSEPlPkFl5z4Ms3yAVw==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+
+@echo off
+mode con: cols=95 lines=40
+title Server-Launch-Manager 
+color F0
+goto first
+
+
+:light
+color F0
+goto start
+
+(
+set /p run=
+set /p cache=
+set /p cfg=
+set /p configstatus=
+)<fmlsettings.bat
+echo %run%
+echo %cache%
+echo %cfg%
+echo %configstatus%
+::if "%configstatus%" == "DONE" goto first
+pause
+
+
+
+:pic
+echo MMMMMMMMMMMMMMMMMMMMMMMMMMmmdddhyyyyyyyyyyyyyhhhddddmNMMMMMMMMMMMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMMMMMNmhyyyyyyyyyyyyyyyyyyyyyyyyyyhhhhhhdmNMMMMMMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMMNhhyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyhyyydmNNMMMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMmdyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyydmmmmmmNMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMNdyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyhdmmmmmmmmNMMMMMMMMMMMMM
+echo MMMMMMMMMMmhyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyhmmmmmmmmmmmmNMMMMMMMMMM
+echo MMMMMMMMNs//osyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyhdddmmmmmmmmdhmNMMMMMMMM
+echo MMMMMMMdo/:::/osyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyhmddmmmmmmmmhhhhNMMMMMMM
+echo MMMMMNy/:::::++/osyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyhmddmmmmmmmmhhhhhdMMMMMM
+echo MMMMMs:::::::os+osyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyhdddmmmmmmmmhhhhhhhMMMMM
+echo MMMNhs+/:::::/:::/+ohdmmddyyyyyyyyyyydddddyyyyyyyyyyyyyyhdddddddddddddddhyhmNMMM
+echo MMMhssso+:::::::::::/shdmmdhysssssssydddddysssssssyyyyyyyddhhhhhhdddddddhyyhhNMM
+echo MMhsssssso/:---::::::://oyyhysssssssydddddysso/.ossyyyyyydhyyo--+yhddddhyo+.:yMM
+echo Mmssssssssso+::::::::::::/+ssssssssyddddddys+-  +ssyyyyyyhys:`   syhhhhyo-`  /mM
+echo Mhyyyyyyysssss+/:::::::::::/+sssyhhhdddddhyo.  -ossyyyyssyo-     oyhhys+.   `+yN
+echo Nddddhhhhhyyyyhys+//:::::::::/+shdddddddhy+.  -osyhhyyyso+.     :syyyo:`   `/shm
+echo dddddddddhhhhhhhhhysoo++/////++oyddddddhy+`  :oyyhhhhhyo/`  `  .+sys/. `  `/shhd
+echo ddddddddddddhdhhhdhhyssssyyyyysosyddddhy/` `:syyhhhhyyo:` `-   /oso:``.` `/syhdd
+echo mmdddddddddddddddhhhyssssyyyyysosyhddhs/` `/syyhhhhhyo-  .:.  -+o/.`.-` `+shdddd
+echo dddddhhddddddddhhhysoooosyyyyysoyhdhhs/` `/shhhhhhhyo.  -//` `//-``::` `+yhddddd
+echo dddddddddddddddhyssssssssyyyyhhhdddhy/` `+yhhddhhhy+.  :++-  -:` -//` `/yhdddddd
+echo mdddmdddddddddhyyysssssssyyyhdmdddhy+` `+syhhddhyy+. `/oo+` `. `:o+.  /shdddmmdd
+echo mddddddddddhyyyysssyyyyyyyhddmmddhy+` `:+ossssyyso` `+sss:    .+oo:  :syhdddmmdd
+echo mmmmmdddddyysyyyyyyyyyyyyddddmdddhy/  `....```.+o- .oyyys:   :osys. -syhddmdmddd
+echo Nmmmmmmddyyyyyyyyyyyyhhddddddmdddhhs-```..-://+os:.oyhhhhs-`/syhhy:-shhdddmddmmN
+echo MmmmmmddhhhhhhyyyyyyhdmddddmmmmmdddhysssyyyhhhhhhhyhhddddhhyhhhdhhhyhdddmmmmmmmM
+echo MNmmdhyhhhhhhhysyyhddddddddddddddddddddddddddddddddddddddddddddddddddddddmmmmmNM
+echo MMNmdhhhhyyyyyyyhddddddddhhyyyhhhhhhhhhhhhhhhhhhhhhhhhhyhhhhhhhhhhhhyyyyhdddmmMM
+echo MMMNmddyyyysyyyhddddddddhyyyyyyyyyyhhhhhhhhhhhyhhyyhhyyyyyyyyhhhhhhhhhhhhdmmmMMM
+echo MMMMNmmhyyyyhdddddddmmddyssyyyyyyssyyyyhhhhhhhhhyyyysssyyyhhhyyyhhhhhhhhhdmmMMMM
+echo MMMMMNmddddddmmmddddddmmhyssysyyyyyyyyyyyyyyyyhyssysssyyyyhhhyhhhhhhhhhhhdmMMMMM
+echo MMMMMMNmdddddmmmmmddddddddhyhyyyyyhhhhyyyyyyhhhhhyhhyyyyhhhhhhhhhhhhddhddNMMMMMM
+echo MMMMMMMNmmmddmmmmmmmdddddddmmddddddddddddddddddddddddddddddddddmdddmmmmmNMMMMMMM
+echo MMMMMMMMMNmdddmmmmmmmmdddddddmmmdddddddddddddddddddddddddddddddmdddddmNMMMMMMMMM
+echo MMMMMMMMMMMmddmmmmmmmmmmmdddddddmmmddddddddddddddddddddddddddddddddmmMMMMMMMMMMM
+echo MMMMMMMMMMMMMNmmmmmmmddddmmdddddddddmddddddddmddddddmmdddddddddddmmMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMNNmmmmmddddmmmdddddddddmmddddddddddddddddddddddmmNMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMMMNmmmmmdmmmmmmdddddddddmmdddddddddddddddmmmNMMMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMMMMMMNNmdmmmmmmmmdddddddddmmmdddddddddmmNMMMMMMMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMMMMMMMMMMNNmmmmmmmmddddddddddmmmmmmNNMMMMMMMMMMMMMMMMMMMMMMMMMM
+pause
+goto start
+
+
+
+
+
+:setup
+cls
+echo ---------------------------------------------
+echo --     Welcome To FiveM Server Launcher    --
+echo --             Configuration               --
+echo ---------------------------------------------
+::echo @echo off > fmlsettings.bat
+pause
+
+:1config
+cls
+echo ----------------------------------------------------------------------------
+echo --          What is the exact file location of your "run.cmd?"            --
+echo --                  Example: C:\FXServer_ESX\run.cmd                      --
+echo ----------------------------------------------------------------------------
+set /p run=: 
+echo %run% +exec server.cfg >> startserver.bat
+
+
+:2config
+cls
+echo ----------------------------------------------------------------------------
+echo --          What is the exact file location of your "cache"               --
+echo --                  Example: C:\FXServer_ESX\cache\                       --
+echo ----------------------------------------------------------------------------
+set /p cache=: 
+echo rd /s /q "%cache%" >> cacheclear.bat
+
+
+:3config
+cls
+echo ----------------------------------------------------------------------------
+echo --        What is the exact file location of your "server.cfg"            --
+echo --                Example: C:\FXServer_ESX\server.cfg                     --
+echo ----------------------------------------------------------------------------
+set /p cfg=: 
+echo %cfg% >> serverconfig.bat
+
+cls
+echo ----------------------------------------------------------------------------
+echo --               The configuration has been completed                     --
+echo --         You will now procede to the Server Launch Manager              --
+echo ----------------------------------------------------------------------------
+echo set configstatus=DONE >> fmlsettings.bat
+echo pause >> fmlsettings.bat
+pause
+goto start
+
+
+
+
+
+
+
+
+
+
+:first
+mode con: cols=81 lines=40
+echo MMhsssssso/:---::::::://oyyhysssssssydddddysso/.ossyyyyyydhyyo--+yhddddhyo+.:yMM
+echo Mmssssssssso+::::::::::::/+ssssssssyddddddys+-  +ssyyyyyyhys:`   syhhhhyo-`  /mM
+echo Mhyyyyyyysssss+/:::::::::::/+sssyhhhdddddhyo.  -ossyyyyssyo-     oyhhys+.   `+yN
+echo Nddddhhhhhyyyyhys+//:::::::::/+shdddddddhy+.  -osyhhyyyso+.     :syyyo:`   `/shm
+echo dddddddddhhhhhhhhhysoo++/////++oyddddddhy+`  :oyyhhhhhyo/`  `  .+sys/. `  `/shhd
+echo ddddddddddddhdhhhdhhyssssyyyyysosyddddhy/` `:syyhhhhyyo:` `-   /oso:``.` `/syhdd
+echo mmdddddddddddddddhhhyssssyyyyysosyhddhs/` `/syyhhhhhyo-  .:.  -+o/.`.-` `+shdddd
+echo dddddhhddddddddhhhysoooosyyyyysoyhdhhs/` `/shhhhhhhyo.  -//` `//-``::` `+yhddddd
+echo dddddddddddddddhyssssssssyyyyhhhdddhy/` `+yhhddhhhy+.  :++-  -:` -//` `/yhdddddd
+echo mdddmdddddddddhyyysssssssyyyhdmdddhy+` `+syhhddhyy+. `/oo+` `. `:o+.  /shdddmmdd
+echo mddddddddddhyyyysssyyyyyyyhddmmddhy+` `:+ossssyyso` `+sss:    .+oo:  :syhdddmmdd
+echo mmmmmdddddyysyyyyyyyyyyyyddddmdddhy/  `....```.+o- .oyyys:   :osys. -syhddmdmddd
+echo Nmmmmmmddyyyyyyyyyyyyhhddddddmdddhhs-```..-://+os:.oyhhhhs-`/syhhy:-shhdddmddmmN
+echo MmmmmmddhhhhhhyyyyyyhdmddddmmmmmdddhysssyyyhhhhhhhyhhddddhhyhhhdhhhyhdddmmmmmmmM
+echo ---------------------------------------------
+echo --     Welcome To Server Launch Manager    --
+echo --      Please select an option below      --
+echo ---------------------------------------------
+echo -- 1. [fivem] Start FiveM Sever            --
+echo -- 2. [cache] Clear Server Cache           --
+echo -- 3. [config] Open Server.cfg             --
+echo -- 4. [data] Open PHPmyadmin               --
+echo -- 5. [update] Check for version updates   --
+echo -- 6. [discord] Join our Discord           --
+echo -- 7. [forum] FiveM Forum                  --
+echo -- 8. [dark] Change window to dark mode    --
+echo -- 9. [setup] Configure this Manager       --
+
+Set "url=https://pastebin.com/raw/a7e9NxLF"
+for %%# in (%url%) do ( set "File=%tmp%\%%~n#.txt" )
+Call :Download "%url%" "%File%"
+If exist "%File%" Type "%File%"
+echo.
+echo --           Your Version= 1.0.1.0         --
+echo ---------------------------------------------
+echo.
+set /p PROGRAM= Enter Command: 
+goto %PROGRAM%
+pause
+
+
+
+
+:start
+cls
+mode con: cols=70 lines=20
+echo ---------------------------------------------
+echo --     Welcome To Server Launch Manager    --
+echo --      Please select an option below      --
+echo ---------------------------------------------
+echo -- 1. [fivem] Start FiveM Sever            --
+echo -- 2. [cache] Clear Server Cache           --
+echo -- 3. [config] Open Server.cfg             --
+echo -- 4. [data] Open PHPmyadmin               --
+echo -- 5. [update] Check for version updates   --
+echo -- 6. [discord] Join our Discord           --
+echo -- 7. [forum] FiveM Forum                  --
+echo -- 8. [dark] Change window to dark mode    --
+echo ---------------------------------------------
+echo.
+set /p PROGRAM= Enter Command: 
+goto %PROGRAM%
+
+
+
+
+:password
+cls
+set /p PASSWORD= What do you want your password to be?: 
+echo %PASSWORD% > pass.txt
+pause
+goto start
+
+
+
+
+:check
+cls
+set /p PASSWORD1= What is your password?
+
+for /f "Delims=" %%a in (pass.txt) do (
+
+set TEXT=%%a
+
+)
+
+if %PASSWORD1%==%TEXT% goto start
+echo you are wrong!!!
+pause
+goto start
+
+
+
+:fivem
+mode con: cols=70 lines=20
+cls
+echo ---------------------------------------------
+echo --    Are you sure you want to start?      --
+echo ---------------------------------------------
+echo.
+set /P c=[Y/N]? 
+if /I "%c%" EQU "Y" goto :startfivem
+if /I "%c%" EQU "N" goto :start
+:startfivem
+start startserver.bat +exec server.cfg
+goto start
+
+
+:cache
+mode con: cols=70 lines=20
+cls
+echo ---------------------------------------------
+echo --  Are you sure you want to clear cache?  --
+echo ---------------------------------------------
+echo.
+set /P c=[Y/N]? 
+if /I "%c%" EQU "Y" goto :clear
+if /I "%c%" EQU "N" goto :start
+:clear
+cls
+call cacheclear.bat
+echo ---------------------------------------------
+echo --             Cache Cleared!              --
+echo ---------------------------------------------
+echo.
+pause
+goto start
+
+
+
+:config
+mode con: cols=70 lines=20
+call serverconfig.bat
+goto start
+
+
+
+:data
+mode con: cols=70 lines=20
+start "" http://localhost/phpmyadmin/
+goto start
+
+:discord
+mode con: cols=70 lines=20
+start "" https://discord.gg/DvZwE2z
+goto start
+
+:forum
+mode con: cols=70 lines=20
+start "" https://forum.fivem.net/
+goto start
+
+
+:update
+mode con: cols=70 lines=20
+cls
+Set "url=https://pastebin.com/raw/2WYhEVKQ"
+for %%# in (%url%) do ( set "File=%tmp%\%%~n#.txt" )
+Call :Download "%url%" "%File%"
+If exist "%File%" Type "%File%"
+echo.
+echo --         Your Version= 1.0.1.0           --
+echo ---------------------------------------------
+set /P c=Update [Y/N]? 
+if /I "%c%" EQU "Y" goto :updaterun
+if /I "%c%" EQU "N" goto :start
+::*********************************************************************************
+:Download <url> <File>
+Powershell.exe -command "(New-Object System.Net.WebClient).DownloadFile('%1','%2')"
+exit /b
+::*********************************************************************************
+
+
+:updaterun
+mode con: cols=70 lines=20
+start "" https://github.com/Windy315/Server-Launch-Manager
+goto start
+
+:dark
+mode con: cols=70 lines=20
+color 07
+goto start
+
+:end
+:exit
